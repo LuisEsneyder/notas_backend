547df777d25082c7e2021ba87c047b5617257d9f
Luis Esneyder Fernández <luisexneider1999@gmail.com>
2023-04-03 22:28:12 -0500

    uibuild

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ const app = express()
 
 //se crea un middleware, para poder observar lo que se envia al server
 const requestLoogger = (request, response, next) => {
-    console.log('Method:', request.method);
-    console.log('Path:', request.path);
-    console.log('body:', request.body);
-    console.log('---');
-    next()
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('body:', request.body)
+  console.log('---')
+  next()
 }
 // middleware para cuando se intenta acceder a una ruta que no existe, este se debe poner al final de la rutas
 const unKnowEndpoind = (request, response) => {
-    response.status(400).send({error: 'unknow endpoint'})
+  response.status(400).send({ error: 'unknow endpoint' })
 }
 //middelwere para manejar los errores, debe ser el ultimo en cargar
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
-    if(error.name === 'CastError'){
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    if(error.name ==='ValidationError'){
-        return response.status(400).json({ error: error.message })
-    }
-    next(error)
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 //se agregan los middleware al app
 //para poder acceder a la información en formato json
@@ -41,56 +41,66 @@ app.use(requestLoogger)
 //se crean las rutas para hacer las peticiones:
 // ruta home de la aplicaión
 app.get('/', (request, response) => {
-    response.send('<h1>Hello world!</h1>')
+  response.send('<h1>Hello world!</h1>')
 })
 //ruta para obtener las notas
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
 })
 //ruta para obtener una sola nota
 app.get('/api/notes/:id', (request, response, next) => {
-    const id = request.params.id
-    Note.findById(id).then(note => {
-        if (note){
-            response.json(note)
-            return
-        }
-        response.status(400).end()
-    }).catch(error => next(error))
-
+  const id = request.params.id
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+        return
+      }
+      response.status(400).end()
+    })
+    .catch((error) => next(error))
 })
 //ruta para eliminar una sola nota usando el id
 app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id).then(result => {
-        response.status(202).end()
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(202).end()
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 //ruta para crear una nueva nota
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
-    //se instancia el modelo para crear una nota    
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-    }) 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    }).catch(error => next(error))
+  const body = request.body
+  //se instancia el modelo para crear una nota
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    .catch((error) => next(error))
 })
 //ruta para actualizar
-app.put('/api/notes/:id',(request, response, next) => {
-    const {content, important} = request.body
-    const note = {
-        content,
-        important
-    }
-    Note.findByIdAndUpdate(request.params.id, note, {new: true, runValidators: true, context:'query'}).then(result => {
-        response.json(result)
-    }).catch(error => next(error))
-    
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+  const note = {
+    content,
+    important,
+  }
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((result) => {
+      response.json(result)
+    })
+    .catch((error) => next(error))
 })
 
 //se llama el middleware cuando se intenta acceder a una ruta que no existe
@@ -98,5 +108,5 @@ app.use(unKnowEndpoind)
 app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log((`Server running on port ${PORT}`))
+  console.log(`Server running on port ${PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -4,30 +4,32 @@ mongoose.set('strictQuery', false)
 //definimos la url
 const url = process.env.MONGODB_URL
 //nos conectamos
-console.log('connecting to', url);
-mongoose.connect(url).then(result => {
-    console.log('connected to MongoDB');
-}).catch(error => {
-    console.log('error conecting to MongoDB', error.message);
-})
+console.log('connecting to', url)
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error conecting to MongoDB', error.message)
+  })
 //creamos el esquema para notas
 const noteSchema = mongoose.Schema({
-    content:{
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    important: Boolean,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
 })
 //modificamos el guardado de los datos
 
-noteSchema.set('toJSON',{
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 })
 
 module.exports = mongoose.model('Note', noteSchema)
-
